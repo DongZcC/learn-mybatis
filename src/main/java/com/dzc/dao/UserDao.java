52638c0f27ec472eb56482d5b9322eeffd0ce912
dongZzc <dongZzc@foxmail.com>
2018-01-05 17:29:28 +0800

    first test complete

--- src/main/java/com/dzc/dao/UserDao.java
+++ src/main/java/com/dzc/dao/UserDao.java
@@ -1,10 +1,17 @@
 package com.dzc.dao;
 
+import com.dzc.entity.User;
+
+import java.util.List;
+
 /**
  * 功能说明: <br>
  * 系统版本: v1.0<br>
  * 开发人员: @author dongzc15247<br>
  * 开发时间: 2018-01-05<br>
  */
-public class UserDao {
+public interface UserDao {
+    void insert(User user);
+    User findUserById(int userId);
+    List<User> findAllUsers();
 }
---
+++ src/main/test/UserDaoTest.java
@@ -0,0 +1,41 @@
+import com.dzc.dao.UserDao;
+import com.dzc.entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * 功能说明: <br>
+ * 系统版本: v1.0<br>
+ * 开发人员: @author dongzc15247<br>
+ * 开发时间: 2018-01-05<br>
+ */
+public class UserDaoTest {
+
+    @Test
+    public void findUserById() {
+        SqlSession sqlSession = getSessionFactory().openSession();
+        UserDao userMapper = sqlSession.getMapper(UserDao.class);
+        User user = userMapper.findUserById(2);
+        Assert.assertNull("没找到数据", user);
+    }
+
+    //Mybatis 通过SqlSessionFactory获取SqlSession, 然后才能通过SqlSession与数据库进行交互
+    private static SqlSessionFactory getSessionFactory() {
+        SqlSessionFactory sessionFactory = null;
+        String resource = "configuration.xml";
+        try {
+            sessionFactory = new SqlSessionFactoryBuilder().build(Resources
+                    .getResourceAsReader(resource));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
+
+}
